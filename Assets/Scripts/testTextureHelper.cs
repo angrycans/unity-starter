950c824e7a335d4b4fc962f4fef59a48ddaec466
angrycans <angrycans@gmail.com>
2017-04-14 11:52:14 +0800

    init

---
+++ Assets/Scripts/testTextureHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testTextureHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TextureHelper textureHelper=new TextureHelper();
+		textureHelper.CreateTextureInEditor(new Color(),"Assets/Textures/test.png");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
